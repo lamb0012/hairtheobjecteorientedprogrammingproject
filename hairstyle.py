a812695fd8284c591f67910c42d581a583d9246c
lamb0012 <jerryweng104@gmail.com>
2026-05-31 18:47:04 +0200

    Added the if statement for the output

--- hairstyle.py
+++ hairstyle.py
@@ -18,3 +18,21 @@ class toomuch_hair():
     def style(self):
         self.style = "forest hair"
         print("You'll be having all the hair you like.")
+
+style = input("What style would you like : (/curly/straight/fluffy/bald/forest) ")
+
+if style == "curly":
+    hair = curly_hair()
+elif style == "straight":
+    hair = straight_hair()
+elif style == "fluffy":
+    hair = fluffy_hair()
+elif style == "bald":
+    hair = no_hair()
+elif style == "forest":
+    hair = toomuch_hair()
+else :
+    print ("Sir, it's not on the menu...")
+
+if hasattr(hair, "style"):
+    hair.style()
