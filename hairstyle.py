f20aec728ccc8af539ed072f4c369008930fb58c
lamb0012 <jerryweng104@gmail.com>
2026-05-31 18:46:27 +0200

    Attributing class for hair styles

---
+++ hairstyle.py
@@ -0,0 +1,20 @@
+class curly_hair():
+    def style(self):
+        self.style = "curly hair"
+        print("You'll be curly.")
+class straight_hair():
+    def style(self):
+        self.style = "straight hair"
+        print("You'll be straight.")
+class fluffy_hair():
+    def style(self):
+        self.style = "fluffy hair"
+        print("You'll be fluffy.")
+class no_hair():
+    def style(self):
+        self.style = "bald"
+        print("You'll be bald.")
+class toomuch_hair():
+    def style(self):
+        self.style = "forest hair"
+        print("You'll be having all the hair you like.")
